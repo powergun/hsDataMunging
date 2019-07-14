3963e21d2406758c5ce61a768f5311f9430fa1cd
Wei Ning <wei@canva.com>
2019-07-14 15:45:54 +1000

    wip

--- scripts/strings/split.hs
+++ scripts/strings/split.hs
@@ -1,1 +1,6 @@
 -- TODO: see data_munging/fileIO/search_in_file
+
+-- see also hsAlgorithms/maps
+-- there is a 3rd party package called "split" that provides
+-- utility functions to split strings, such as splitOn, splitAt
+
