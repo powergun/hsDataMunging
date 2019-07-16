920025fc51b56368e95625fc248e82c25bc0aecc
Wei Ning <wei@canva.com>
2019-07-16 18:19:33 +1000

    wip

--- scripts/parsing/parser_monad.hs
+++ scripts/parsing/parser_monad.hs
@@ -38,8 +38,72 @@ demoParserAsFunctor = do
   print $ parse (fmap id atom) "iddqd"
   print $ parse (fmap toUpper atom) "iddqd"
 
+-- P180
+-- <*> applies a parser that returns a function to a parser that 
+-- returns an argument to give a parser that returns the result
+-- of applying the function to the argument, and only succeeds
+-- if all the components succeed
+-- for example, a parser that consumes three characters, discards 
+-- the second and returns the first and third as a pair can now 
+-- be defined in applicative style
+instance Applicative AtomicParser where
+  pure v = AtomicParser (\s -> [(v, s)])
+  pf <*> px =
+    let newParserFunc s = 
+          case (runParser pf s) of
+            [] -> []
+            [(f, out)] -> runParser (fmap f px) out
+    in AtomicParser newParserFunc
+
+demoParserAsApplicative :: IO ()
+demoParserAsApplicative = do
+  print $ runParser (pure "X") "iddqd"
+  let three :: AtomicParser (Char, Char)
+      three = pure g <*> atom <*> atom <*> atom
+              -- func    arg      arg      arg
+              where g x y z = (toUpper x, toUpper z)
+  print $ runParser three "iddqd"
+  --      ^^^^^^^^^^^^^^^ func arg arg arg 
+  -- P180
+  -- the applicative machinery automatically ensures that the
+  -- above parser fails if the input string is too short, with
+  -- the need to detect or manage this ourselves
+  print $ runParser three "ad"
+
+-- P180
+-- parser p >>= f fails if the application of the parser p
+-- to the input string s fails, and otherwise applies the 
+-- function f to the result value v to given another parser 
+-- f v
+-- which is then applied to the output string out that was 
+-- produced by the first parser to give the final result 
+instance Monad AtomicParser where
+  return = pure
+  p >>= f =
+    let newParserFunc s = 
+          case (runParser p s) of
+            [] -> []
+            [(v, out)] -> runParser (f v) out
+    in AtomicParser newParserFunc
+
+-- because AtomicParser is a monadic type, the do notation can
+-- now be used to sequence parsers and process their result 
+-- values
+demoParserMonad :: IO ()
+demoParserMonad = do
+  print $ runParser (return 'L') "idkfa"
+  let three :: AtomicParser (Char, Char)
+      three = do
+        x <- atom
+        atom
+        z <- atom
+        return (x, z)
+  print $ runParser three "thereisnospoon"
+
 main :: IO ()
 main = do
   demoAtomicParser
   demoParserAsFunctor
+  demoParserAsApplicative
+  demoParserMonad
   
