ece98e9c77ac6377b4754ad9e404c4758bb53dd0
Wei Ning <wei@canva.com>
2019-07-19 23:14:58 +1000

    wip

---
+++ scripts/parsing/parsec.hs
@@ -0,0 +1,11 @@
+#!/usr/bin/env stack runghc
+
+-- real world haskell P/423
+
+import           Text.Parsec
+import           Text.Parsec.Error
+import           Text.Parsec.Pos
+
+main :: IO ()
+main = do
+  print 1
