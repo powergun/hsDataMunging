5684296aa787bfea988490d1d29713dc805a828d
Wei Ning <wei@canva.com>
2019-07-20 19:19:53 +1000

    wip

--- scripts/parsing/parsec_tfvars.hs
+++ scripts/parsing/parsec_tfvars.hs
@@ -1,19 +1,35 @@
 #!/usr/bin/env stack runghc
 
+import           Control.Monad
+import           System.IO
 import           Text.ParserCombinators.Parsec
 
-import           System.IO
+data Element = Null
+             | Comment { comment :: String }
+             | Variable { name :: String, value :: String }
+             deriving (Eq, Show)
 
 tfvarFile = endBy line eol
-line = sepBy variable (char '=')
-variable = many (noneOf "=\n")
-eol = char '\n'
+line = p_comment <|> p_variable
 
-parseTfvars :: String -> Either ParseError [[String]]
-parseTfvars input =
-  parse tfvarFile "(unknown)" input
+p_variable :: CharParser () Element
+p_variable = do
+  name <- many p_char
+  value <- (char '=' >> many p_char)
+  return Variable { name = name, value = value }
+
+p_comment :: CharParser () Element
+p_comment = do
+  char '#'
+  comment <- many p_char
+  return Comment { comment = comment }
+
+p_char = noneOf "\n="
+
+eol = char '\n'
 
 main :: IO ()
 main = do
-  content <- readFile "terraform.tfvars"
-  print $ parseTfvars content
+  -- content <- readFile "terraform.tfvars"
+  parseTest tfvarFile "#foo variable\nfoo=bar\n#map name\nmap=e1m1\n"
+  parseTest tfvarFile "#foo variable\n\nfoo=bar\n#map name\nmap=e1m1\n"
---
+++ scripts/parsing/parsec_urlencode.hs
@@ -0,0 +1,48 @@
+#!/usr/bin/env stack runghc
+
+import           Numeric
+import           Text.ParserCombinators.Parsec
+
+-- real world haskell P/433
+
+-- each key-value pair is separated by the & character
+-- HTTP spec is unclear about whether a key must have an
+-- associated value; need to be able to distringuish between
+-- no value and empty value
+p_query :: CharParser () [(String, Maybe String)]
+p_query = p_pair `sepBy` char '&'
+
+p_pair :: CharParser () (String, Maybe String)
+p_pair = do
+  -- many1 ~ some; many1 can fail
+  name <- many1 p_char
+  -- optionMaybe modifies the behavior of a parser
+  -- if the parser fails, optionMaybe does not: it returns Nothing
+  -- otherwise it wraps the parser's successful result with Just
+  -- it gives us the ability to distinguish between "no value"
+  -- and "empty value"
+  value <- optionMaybe (char '=' >> many p_char)
+  return (name, value)
+
+p_char :: CharParser () Char
+p_char =   oneOf urlBaseChars
+       <|> (char '+' >> return ' ')
+       <|> p_hex
+
+urlBaseChars = ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9'] ++ "$-_.!*'(),"
+
+p_hex :: CharParser () Char
+p_hex = do
+  char '%'
+  a <- hexDigit
+  b <- hexDigit
+  let ((d, _):_) = readHex [a, b]
+  return . toEnum $ d
+
+demoParseURL :: IO ()
+demoParseURL = do
+  parseTest p_query "foo=bar&a%21=b+c"
+
+main :: IO ()
+main = do
+  demoParseURL
