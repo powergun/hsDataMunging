1b1c06763208a29c53318fde4200a3cfa017e325
Wei Ning <macgnw@gmail.com>
2019-07-22 21:53:09 +1000

    wip

--- scripts/parsing/simple_parsec.hs
+++ scripts/parsing/simple_parsec.hs
@@ -1,8 +1,11 @@
 #!/usr/bin/env stack runghc
 
-import Text.ParserCombinators.Parsec hiding ((<|>), many)
-import Control.Applicative
-import Control.Monad
+-- source 
+-- parse stuff in haskell (meetup video)
+
+import Text.ParserCombinators.Parsec
+-- import Control.Applicative
+-- import Control.Monad
 
 demoMatchWord :: IO ()
 demoMatchWord = do
@@ -12,12 +15,37 @@ demoMatchWord = do
     matchWord :: Parser String
     matchWord = string "true"
 
+demoMatchTrue :: IO ()
+demoMatchTrue = do
+  let err = "failed"
+      parser = string "true"
+  print $ parse parser err "true"
+  print $ parse parser err "false"
+
+demoAlwaysTrue :: IO ()
+demoAlwaysTrue = do
+  let parser = pure True :: Parser Bool
+      parser' = string "true"
+  print $ parse (parser' *> parser) "..." "iddqd"
+  -- if the first succeeds, carry on to the next parser
+  print $ parse (parser' *> parser) "..." "true"
+
 demoMatchBool :: IO ()
 demoMatchBool = do
-  print 1
+  let parser = ((string "true") *> (pure True))
+               <|> ((string "false") *> (pure False))
+  print $ parse parser "..." "true"
+  print $ parse parser "..." "false"
+  print $ parse parser "..." "123"
+
+demoMatchStringLiteral :: IO ()
+demoMatchStringLiteral = do
+  -- interesting syntax!!
+  -- the definition of <* is even more entertaining!!
+  let parser = (char '"') *> (many (noneOf "\"")) <* (char '"')
+  print $ parse parser "..." "thereisacow"
+  print $ parse parser "..." "\"there is a cow + 1\""
 
 main :: IO ()
 main = do
-  demoMatchWord
-  demoMatchBool
-
+  demoMatchStringLiteral
