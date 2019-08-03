18653aaf9fec3c7519415e28de8ccc2966d2a15e
Wei Ning <macgnw@gmail.com>
2019-08-04 08:14:37 +1000

    wip

--- sed/app/sed/Main.hs
+++ sed/app/sed/Main.hs
@@ -1,88 +1,7 @@
 module Main where
 
-import qualified Control.Monad       as M
-import qualified Control.Monad.State as Ms
--- what is zipper: https://wiki.haskell.org/Zipper
-import qualified Data.List.Zipper    as Z
-import qualified Data.Text           as T
-import qualified Data.Text.IO        as TIO
-import qualified Text.Regex          as Regex
-
--- source
--- sed implementation in haskell video
-
-data SedState = SedState {
-  line         :: Int
-, input        :: Z.Zipper T.Text
-, patternSpace :: T.Text
-, holdSpace    :: T.Text
-} deriving (Show)
-
-data Command = Print
-             | Next
-             | Delete
-             | Substitute String String String
-             deriving (Show)
-
--- reason for using Text type: Text allows utf-8 encoding, whereas
--- String is ascii
--- wrapping the monadial function! use evalState to pull the result
-sed :: [Command] -> T.Text -> T.Text
-sed cmds t =
-  -- note: I can not use t as the default SedState! it must be
-  -- constructed; see the definition of defaultState function
-  Ms.evalState (runCommands cmds) (defaultState t)
-
-defaultState :: T.Text -> SedState
-defaultState t =
-  SedState 1 (Z.delete z) (Z.cursor z) (T.singleton '\n')
-  where
-    z = Z.fromList (T.lines t)
-
-runCommands :: [Command] -> Ms.State SedState T.Text
--- because this function needs to "return T.Text" instead of
--- Ms.State (the type returned by "calling return()"), I need
--- an accessor here, and compose it with return
-runCommands cmds = do
-  M.mapM_ runCommand cmds
-  ss <- Ms.get
-  if Z.endp $ input ss
-  then (return . T.unlines . Z.toList) (input ss)
-  else runCommand Next >> runCommands cmds
-
--- note that the video uses the bind command >>= so it has
--- to define a new lambda function for each new action
--- \\\\ this is better \\\\
--- ss <- Ms.get
--- ss { output = output ss `T.append` patternSpace ss }
--- \\\\ this is even better \\\\
-runCommand :: Command -> Ms.State SedState ()
-runCommand Print =
-  Ms.modify $ \ss -> ss { input = Z.push (patternSpace ss) (input ss) }
-  -- ss is not seen in "where" block; I have to use let..in
-
-runCommand Next = Ms.modify $ \ss ->
-  ss { line = line ss + 1
-     , input = Z.delete (input ss)
-     , patternSpace = Z.cursor (input ss) }
-
-runCommand Delete = Ms.modify $ \ss -> ss { patternSpace = T.empty }
-
-runCommand (Substitute pat sub flags) = Ms.modify $ \ss ->
-  -- replacement string takes \\0, \\1, \\2 that refer to the capture
-  -- perl deals with this in a similar way
-  -- perl -n -E 's/(\d+).(\d+)/\1--\2/g and say' <<<"234.123 3241.213123"
-  -- 234--123 3241--213123
-  let newText = T.pack (Regex.subRegex regex src sub)
-      regex = Regex.mkRegex pat
-      src = T.unpack (patternSpace ss)
-  in ss { patternSpace = newText }
-
--- the naive "unlines . lines" concatenation logic for T.Text
--- is O(n^2); therefore I need to introduce a O(n) concat
--- it also needs to account for the newline character
-(<+>) :: T.Text -> T.Text -> T.Text
-(<+>) t1 t2 = t1 `T.append` T.cons '\n' t2
+import qualified Data.Text.IO as TIO
+import           SedLib       (Command (..), sed)
 
 main :: IO ()
 -- Prelude interact accepts String -> String function, therefore
@@ -90,4 +9,5 @@ main :: IO ()
 -- test case:
 -- Substitute "[0-9]+" "classified(\\0)"
 -- py -c "[print('there is %d some' % id(n)) for n in range(10)]" | hsed
-main = TIO.interact $ sed [Substitute "[0-9]+" "classified(\\0)" "", Print]
+main =
+  TIO.interact $ sed [Substitute "[0-9]+" "classified(\\0)" "", Print]
---
+++ sed/src/SedLib.hs
@@ -0,0 +1,87 @@
+module SedLib
+  ( Command(..)
+  , sed ) where
+
+import qualified Control.Monad       as M
+import qualified Control.Monad.State as Ms
+-- what is zipper: https://wiki.haskell.org/Zipper
+import qualified Data.List.Zipper    as Z
+import qualified Data.Text           as T
+import qualified Data.Text.IO        as TIO
+import qualified Text.Regex          as Regex
+
+-- source
+-- sed implementation in haskell video
+
+data SedState = SedState {
+  line         :: Int
+, zipper       :: Z.Zipper T.Text
+, patternSpace :: T.Text
+, holdSpace    :: T.Text
+} deriving (Show)
+
+data Command = Print
+             | Next
+             | Delete
+             | Substitute String String String
+             deriving (Show)
+
+-- reason for using Text type: Text allows utf-8 encoding, whereas
+-- String is ascii
+-- wrapping the monadial function! use evalState to pull the result
+sed :: [Command] -> T.Text -> T.Text
+sed cmds t =
+  -- note: I can not use t as the default SedState! it must be
+  -- constructed; see the definition of defaultState function
+  Ms.evalState (runCommands cmds) (defaultState t)
+
+defaultState :: T.Text -> SedState
+defaultState t =
+  SedState 1 (Z.delete z) (Z.cursor z) (T.singleton '\n')
+  where
+    z = Z.fromList (T.lines t)
+
+runCommands :: [Command] -> Ms.State SedState T.Text
+-- because this function needs to "return T.Text" instead of
+-- Ms.State (the type returned by "calling return()"), I need
+-- an accessor here, and compose it with return
+runCommands cmds = do
+  M.mapM_ runCommand cmds
+  ss <- Ms.get
+  if Z.endp $ zipper ss
+  then (return . T.unlines . Z.toList) (zipper ss)
+  else runCommand Next >> runCommands cmds
+
+-- note that the video uses the bind command >>= so it has
+-- to define a new lambda function for each new action
+-- \\\\ this is better \\\\
+-- ss <- Ms.get
+-- ss { output = output ss `T.append` patternSpace ss }
+-- \\\\ this is even better \\\\
+runCommand :: Command -> Ms.State SedState ()
+runCommand Print =
+  Ms.modify $ \ss -> ss { zipper = Z.push (patternSpace ss) (zipper ss) }
+  -- ss is not seen in "where" block; I have to use let..in
+
+runCommand Next = Ms.modify $ \ss ->
+  ss { line = line ss + 1
+     , zipper = Z.delete (zipper ss)
+     , patternSpace = Z.cursor (zipper ss) }
+
+runCommand Delete = Ms.modify $ \ss -> ss { patternSpace = T.empty }
+
+runCommand (Substitute pat sub flags) = Ms.modify $ \ss ->
+  -- replacement string takes \\0, \\1, \\2 that refer to the capture
+  -- perl deals with this in a similar way
+  -- perl -n -E 's/(\d+).(\d+)/\1--\2/g and say' <<<"234.123 3241.213123"
+  -- 234--123 3241--213123
+  let newText = T.pack (Regex.subRegex regex src sub)
+      regex = Regex.mkRegex pat
+      src = T.unpack (patternSpace ss)
+  in ss { patternSpace = newText }
+
+-- the naive "unlines . lines" concatenation logic for T.Text
+-- is O(n^2); therefore I need to introduce a O(n) concat
+-- it also needs to account for the newline character
+(<+>) :: T.Text -> T.Text -> T.Text
+(<+>) t1 t2 = t1 `T.append` T.cons '\n' t2
