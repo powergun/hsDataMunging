4c9c042e24e867021a0ae73d4c1a7b6449458f01
Wei Ning <macgnw@gmail.com>
2019-08-03 12:11:46 +1000

    wip

--- sed/app/sed/Main.hs
+++ sed/app/sed/Main.hs
@@ -1,5 +1,17 @@
 module Main where
 
-main :: IO () 
-main = do
-  print 1
+import qualified Data.Text    as T
+import qualified Data.Text.IO as TIO
+
+-- source
+-- sed implementation in haskell video
+
+-- reason for using Text type: Text allows utf-8 encoding, whereas
+-- String is ascii
+sed :: T.Text -> T.Text
+sed t = t
+
+main :: IO ()
+-- Prelude interact accepts String -> String function, therefore
+-- I need to use the TIO version of interact
+main = TIO.interact sed
