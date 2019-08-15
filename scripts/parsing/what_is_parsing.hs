0ea203c8015815c48d2e3aee458a9c4fd78141e9
Wei Ning <macgnw@gmail.com>
2019-08-15 21:40:33 +1000

    wip

---
+++ scripts/parsing/what_is_parsing.hs
@@ -0,0 +1,6 @@
+{-
+programming haskell L5057
+a parser   is a program that takes a string of characters as input,
+and produces some form of tree that makes the syntactic structure of 
+the string explicit.
+-}
