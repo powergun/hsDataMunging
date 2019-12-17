d7705501fb9f5b16a894c5404b4bc974ead89d7d
Wei Ning <macgnw@gmail.com>
2019-12-18 07:32:47 +1100

    csv: depednencies

--- csvmunging/src/Lib.hs
+++ csvmunging/src/Lib.hs
@@ -2,5 +2,24 @@ module Lib
     ( someFunc
     ) where
 
+import           Data.Text (Text)
+import           Data.Time (Day)
+
+data LedgerLine = LedgerLine {
+    date    :: Day
+, amount    :: Amount
+, reference :: Reference
+, category  :: Maybe Category
+} deriving (Eq, Show)
+
+data Category = Category Text
+    deriving (Eq, Show)
+
+data Reference = Reference Text
+    deriving (Eq, Show)
+
+data Amount = Amount Double
+    deriving (Eq, Show)
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
